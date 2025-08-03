fde116225da4ed5fbe672d8c932818011b4c1ff3
Albert Vaillon <80990528+Eviive@users.noreply.github.com>
2025-08-03 15:59:26 +0200

    CI: PR job (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,12 +28,10 @@ flyway {
     url = flyway.url
     user = flyway.user
     password = flyway.password
-    cleanDisabled = flyway.cleanDisabled
 }
 
 checkstyle {
-    toolVersion = "10.17.0"
-    configFile = file("config/checkstyle/config.xml")
+    toolVersion = "10.26.1"
 }
 
 configurations {
--- src/main/java/dev/albertv/projects/api/util/HashUtils.java
+++ src/main/java/dev/albertv/projects/api/util/HashUtils.java
@@ -1,6 +1,7 @@
 package dev.albertv.projects.api.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.HexUtils;
 import org.springframework.stereotype.Component;
 
 import java.security.MessageDigest;
@@ -10,26 +11,14 @@ import java.security.NoSuchAlgorithmException;
 @Slf4j
 public final class HashUtils {
 
-    public String sha256Hex(String message) {
+    public String sha256Hex(final String message) {
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-            return hex(messageDigest.digest(message.getBytes()));
+            final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            return HexUtils.toHexString(messageDigest.digest(message.getBytes()));
         } catch (NoSuchAlgorithmException e) {
             log.error(e.getMessage(), e);
         }
         return null;
-    }
-
-    private String hex(byte[] bytes) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (byte b : bytes) {
-            stringBuilder.append(
-                Integer.toHexString((b & 0xFF) | 0x100),
-                1,
-                3
-            );
-        }
-        return stringBuilder.toString();
     }
 
 }
